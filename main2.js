474bf86e6a727fadee1adee9a894b852d313380b
pradipta deska pryanda <87347145+pdp3012@users.noreply.github.com>
2023-01-11 08:32:30 +0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,43 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Selamat pagiii dedek", "Maaf yaa udah ganggu kamu", "Udah nyita waktu istirahat kamu juga.", "Disini aku ga bermaksud buat minta maaf soal masalah kmarin, intinya aku salah.", 
+        "semua hal, baik komunikasi termasuk obrolan kita juga", "Kamu pasti tau kn kita kemarin seperti apa", "anak SD aja tau itu bukan hal yang wajar dalam pertemanan",
+        "kita sudah melangkah jauh dan sangat jauh kemarin, plan dan pemikiran aku kedepan juga sudah bukan semata - mata buat aku aja", "namun juga buat kamu",
+        "aku ngerti kok perasaan kamu kemarin, tapi masih ada kesempatan buat perbaikin ini semua", "okeyy kalo emang kamu belum rela",
+        "tapi setidaknya kamu bisa kasi penjelasan hal apa yang bikin kamu balas pesan aku cukup dengan sewajarnya aja", "dan tolong kasi aku kesempatan buat perbaikin",
+        "kalo kamu bingung caranya gimana, kamu cukup fastrespon aja", "kalo emng kamu nya terganggu, kamu bisa bilang ke aku biar kita bisa cari waktu untuk berdua aja",
+        "waktu aku juga banyak, aku bisa denger segala keluh kesah kamu tanpa perlu takut kamu di judge","Remember thiss,  i always be here",
+        "ALWAYSSS", "When u have a bad day","hen nobody appreciate everythngs u do","when u at your lowest point","i always be here",
+        "Even tho u are the who left me at the end.","Karena apa?","Karena aku udah janji dengan diri aku sendiri aku ga akan pernah ninggalin kamu."
+        ,"aku udah nyaman dan terlalu nyaman dengan keadaan sperti kemarin", "asal kamu tau, itu hari yang terindah yang pernah aku rasain dari perempuan selain keluarga aku",
+        "mulai dari dipeduliin","disuru hati hati", "dicemburuin kalo friendly sama cewe lain walaupun ga punya rasa samsek,","Hingga sampai pada suatu pagi perempuan itu beri ucapan `morning ka ryan yg lcuk'",
+        "sampai sekarang aku berdoa ucapan itu ada kembali ketika aku terbangun","tapi nyatanya TIDAK","padahal ucapan itu yang selalu aku tunggu, bukan cuman ucapan nya tapi juga orang nya dengan segala keunikan dan kelebihan yang ga dipunya sama perempuan lain",
+        "Ya, itu kamu","yang selalu aku tunggu","yang selalu aku prioritasin setelah orangtua aku","semua memori itu udh terlanjur masuk ke diri aku, jadi tolong jangan bikin hilang gitu aja",
+        "the lastt, biar ga kaku amat nihh ya","aku punya tebak - tebakan","Mie mie apa yang bikin salting hayoo?","ya mielikin kamu dongg 🥰🥰🥰","maaf ya ga nyambung","soalnya yang nyambung cuman cinta aku ke kamuuu😘😘😘😘"],
+       // strings: ["Disini aku ga bermaksud buat minta maaf soal masalah kmarin, intinya aku salah", "cuman aku disini mau buat perbaikin semuanya."]
+        typeSpeed: 110
+    });
+}
+
+
+
+// kalo emng kamu nya terganggu, kamu bisa bilang ke aku biar kita bisa cari waktu untuk berdua aja. 
+// waktu aku juga banyak, aku bisa denger segala keluh kesah kamu tanpa perlu takut kamu di judge
+// Remember thiss,  i always be here. ALWAYSSS. When u have a bad day, when nobody appreciate everythngs u do, 
+//when u at your lowest point, i always be here. Even tho u are the who left me at the end.
+// Karena apa? Karena aku udah janji aku ga akan pernah ninggalin kamu.
+
+// aku udah nyaman dan terlalu nyaman dengan keadaan sperti kemarin, asal kamu tau, itu hari yang terindah yang pernah aku rasain dari perempuan selain keluarga aku,
+// mulai dipeduliin, disuru hati hati, dicemburuin kalo friendly sama cewe lain walaupun ga punya rasa samsek,
+// Hingga sampai pada suatu pagi perempuan itu beri ucapan "morning ka ryan yg lcuk", sampai sekarang aku berdoa ucapan itu ada kembali ketika aku terbangun,
+// tapi nyatanya tidak, padahal ucapan itu yang selalu aku tunggu, bukan cuman ucapan nya tapi juga orang nya dengan segala keunikan dan kelebihan yang ga dipunya sama perempuan lain
+// Ya, itu kamu, yang selalu aku tunggu, yang selalu aku prioritasin setelah orangtua aku
+// semua memori itu udh terlanjur masuk ke diri aku, jadi tolong jangan bikin hilang gitu aja
+
+// the lastt, biar ga kaku amat aku punya tebak - tebakan nih, mie mie apa yang bikin salting hayoo?
+// ya mielikin kamu dongg 🥰🥰🥰
+// maaf ya ga nyambung, soalnya yang nyambung cuman cinta aku ke kamuuu😘😘😘😘
